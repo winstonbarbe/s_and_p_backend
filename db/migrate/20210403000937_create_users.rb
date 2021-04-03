3afd3dfd990bda876ea502f762597e7c9fdbec1d
winstonbarbe <winston.barbe@gmail.com>
2021-04-02 20:23:26 -0400

    adds user model and controller for signup functionality

---
+++ db/migrate/20210403000937_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :phone_number
+      t.string :pronouns
+      t.text :personal_note
+      t.boolean :admin, default: false
+
+      t.timestamps
+    end
+  end
+end
